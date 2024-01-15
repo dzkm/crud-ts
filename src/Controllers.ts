5c93b7976c5969369bf92c30ae56f468bf279a5a
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-01-14 23:29:34 -0300

    feat/ renamed the files to match our structure

---
+++ src/Controllers.ts
@@ -0,0 +1,3 @@
+import UserController from "./Controllers/UserController";
+
+export const userController = new UserController();
--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { router } from './route.ts';
+import router from './Routers.js';
 
 const app = express();
 app.use(express.json());
